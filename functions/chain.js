9bf13f0bbecff1c7f3a8bd5b2d5b9e25d9db42e0
abygeorge8848 <70239768+abygeorge8848@users.noreply.github.com>
2023-07-17 10:44:33 +0530

    Removed api key

--- functions/chain.js
+++ functions/chain.js
@@ -11,7 +11,7 @@ module.exports = {
     });
     const chain = new SqlDatabaseChain({
       llm: new OpenAI({
-        openAIApiKey: "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",
+        openAIApiKey: "",
         temperature: 0,
       }),
       database: db,
